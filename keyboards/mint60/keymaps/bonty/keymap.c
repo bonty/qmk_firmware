c50306bee2f48af28786c7914c9e64b5c2426bf3
bonty <bonty.shushusha@gmail.com>
2021-08-23 12:00:22 +0900

    GUI right hand

--- keyboards/mint60/keymaps/bonty/keymap.c
+++ keyboards/mint60/keymaps/bonty/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,       KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC, KC_BSLS, \
     LT(1,KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,  KC_ENT, \
     KC_LCTL,      KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,   KC_UP,   MO(3), \
-    KC_ESC, KC_LCTL, KC_LALT, GUI_T(KC_LANG2), SFT_T(KC_SPC), LT(2,KC_ENT), LT(1,KC_LANG1), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT \
+    KC_ESC, KC_LCTL, KC_LALT, GUI_T(KC_LANG2), SFT_T(KC_SPC), LT(2,KC_ENT), GUI_T(KC_LANG1), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT \
   ),
   [1] = LAYOUT( \
     KC_NO,       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, \
