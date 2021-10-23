87264379e7cfe50e6d8a85a586a12c40ad3e8ca3
bonty <bonty.shushusha@gmail.com>
2021-10-23 21:00:04 +0900

    edit mapping

--- keyboards/reviung41/keymaps/bonty/keymap.c
+++ keyboards/reviung41/keymaps/bonty/keymap.c
@@ -10,8 +10,8 @@ enum layer_names {
 };
 
 // Mod-Tap Modifiers
-#define SFTSPC SFT_T(KC_SPC)
 #define ALTTAB ALT_T(KC_TAB)
+#define GUITAB GUI_T(KC_TAB)
 #define CTLESC CTL_T(KC_ESC)
 #define SFTENT SFT_T(KC_ENT)
 
@@ -52,8 +52,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_reviung41(
     ALTTAB,   KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
     CTLESC,   KC_A,     KC_S,     KC_D,     KC_F,      KC_G,               KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_MINS,
-    OS_SFT,   KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,               KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RALT,
-                                            NUMBER,    MT_SPC,   SFTSPC,   SFTENT,   SYMBOL
+    KC_HYPR,  KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,               KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  ONEPASS,
+                                            NUMBER,    MT_SPC,   ALFRED,   SFTENT,   SYMBOL
   ),
 
   [_COLEMAK] = LAYOUT_reviung41(
@@ -79,8 +79,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_NUMBER] = LAYOUT_reviung41(
     XXXXXXX,  KC_F1,    KC_F2,    KC_F3,    KC_F4,     KC_F5,              KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,
-    XXXXXXX,  KC_1,     KC_2,     KC_3,     KC_4,      KC_5,               KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_F12,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_LANG2,  XXXXXXX,            XXXXXXX,  KC_LANG1, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+    XXXXXXX,  KC_1,     KC_2,     KC_3,     KC_4,      KC_LANG2,           KC_LANG1, KC_7,     KC_8,     KC_9,     KC_0,     KC_F12,
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_5,      XXXXXXX,            XXXXXXX,  KC_6,     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
                                             XXXXXXX,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX
   ),
 
