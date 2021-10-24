653f258d5434d8f56173b02ddca61a46b91e1671
bonty <bonty.shushusha@gmail.com>
2021-10-24 22:29:07 +0900

    edit keymap for yabai

--- keyboards/reviung41/keymaps/bonty/keymap.c
+++ keyboards/reviung41/keymaps/bonty/keymap.c
@@ -11,7 +11,6 @@ enum layer_names {
 
 // Mod-Tap Modifiers
 #define ALTTAB ALT_T(KC_TAB)
-#define GUITAB GUI_T(KC_TAB)
 #define CTLESC CTL_T(KC_ESC)
 #define SFTENT SFT_T(KC_ENT)
 
@@ -50,9 +49,9 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_reviung41(
-    ALTTAB,   KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
-    CTLESC,   KC_A,     KC_S,     KC_D,     KC_F,      KC_G,               KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_MINS,
-    KC_HYPR,  KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,               KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  ONEPASS,
+    ALTTAB,   KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     ONEPASS,
+    CTLESC,   KC_A,     KC_S,     KC_D,     KC_F,      KC_G,               KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_RGUI,
+    KC_LALT,  KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,               KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
                                             NUMBER,    MT_SPC,   ALFRED,   SFTENT,   SYMBOL
   ),
 
