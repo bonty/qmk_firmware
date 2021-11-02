6bbbd21517c175573f233f95c2c3c21649f3bd4b
bonty <bonty.shushusha@gmail.com>
2021-11-02 15:40:47 +0900

    one shot modifier

--- keyboards/reviung41/keymaps/bonty/keymap.c
+++ keyboards/reviung41/keymaps/bonty/keymap.c
@@ -54,8 +54,8 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_reviung41(
     KC_ESC,   KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     ONEPASS,
-    KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,      KC_G,               KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_RGUI,
-    KC_LALT,  KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,               KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
+    OS_CTL,   KC_A,     KC_S,     KC_D,     KC_F,      KC_G,               KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  OS_GUI,
+    OS_ALT,   KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,               KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  OS_SFT,
                                             NUMBER,    MT_SPC,   ALFRED,   SFTENT,   SYMBOL
   ),
 
