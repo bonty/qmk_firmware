f4fd75954885fc0e7f217f44a5fc32eaa28b3f9f
bonty <bonty.shushusha@gmail.com>
2022-07-20 22:16:34 +0900

    add mint60 keymap

---
+++ keyboards/mint60/keymaps/bonty/config.h
@@ -0,0 +1,22 @@
+#pragma once
+
+// Layer num
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+
+// Tapping
+#define TAPPING_TERM 185
+#define TAPPING_TERM_PER_KEY
+#define PERMISSIVE_HOLD
+
+// Combos
+#define COMBO_TERM 50
+#define COMBO_COUNT 5
+
+// for light compile
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
+
+#define NO_MUSIC_MODE
+
+#define LAYER_STATE_8BIT
+
